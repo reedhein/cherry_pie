c913d91841da4420613e179e703f6dbebd51bd8c
voodoologic <headley.douglas@gmail.com>
2016-06-13 12:34:03 -0700

    fix code

--- lib/contact_note_to_case.rb
+++ lib/contact_note_to_case.rb
@@ -8,26 +8,32 @@ class ContactNoteToCase
   def perform
     @potential = @sf.find_zoho
     @case      = @sf.cases.first
-    @chatters  = @sf.chatters || @case.try(:chatters)
+    @chatters  = @sf.chatters
     @contact   = @potential.contacts.first
     @notes     = @contact.notes
     puts "processing: #{uniq_notes.count} notes"
-    count = @uniq_notes.count
+    count = uniq_notes.count
     puts @sf.id
-    @uniq_notes.each_with_index do |note, i|
+    uniq_notes.each_with_index do |note, i|
       puts "#{i + 1} of #{count}"
       Utils::SalesForce::FeedItem.create_from_zoho_note(note, @sf)
-      Utils::SalesForce::FeedItem.create_from_zoho_note(note, @case) if @case && @sf.cases.count == 1
       note.mark_migration_complete(:note)
+    end
+    if @case && @sf.cases.count == 1
+      uniq_notes(@case.chatters).each_with_index do |note, i|
+        puts "#{i + 1} of #{count} for case"
+        Utils::SalesForce::FeedItem.create_from_zoho_note(note, @case)
+        note.mark_migration_complete(:note)
+      end
     end
     @case.try(:mark_migration_complete,:notes)
     @sf.mark_migration_complete(:notes)
   end
 
-  def uniq_notes
+  def uniq_notes(chatters = @chatters)
     @uniq_notes ||= @notes.delete_if do |n|
       # n.note_migration_complete? ||
-        @chatters.detect do |c|
+        chatters.detect do |c|
           n.note_content.squish == Nokogiri::HTML(c.body).text.gsub('::FROM ZOHO::', '').gsub(/AUTHORED BY \(.+\)/, '').squish
         end
     end
